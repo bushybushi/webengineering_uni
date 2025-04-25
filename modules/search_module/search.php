c7943aee1e3851f219e2c30e3a48a2697ef66cfd
antonis10 <andreou261@gmail.com>
2025-04-25 15:29:06 +0300

    Update search.php

--- modules/search_module/search.php
+++ modules/search_module/search.php
@@ -320,7 +320,10 @@ $positions = $positionStmt->fetchAll(PDO::FETCH_COLUMN);
                     {
                         targets: [0, 2], // Person Details (0) and Submission Date (2)
                         orderable: true,
-                        orderSequence: ['asc', 'desc', '']
+                        orderSequence: ['asc', 'desc', ' '],
+                        orderData: function(idx) {
+                            return idx === ' ' ? [] : [[idx, 'asc']];
+                        }
                     },
                     {
                         targets: [1, 3, 4], // Title, Political Affiliation, and Actions
