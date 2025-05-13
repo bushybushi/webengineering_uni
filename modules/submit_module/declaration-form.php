3f201aedec5360b0afc6fe16e51d96dc2e953384
Stylianos Kyprianou <43478713+stylianosk0@users.noreply.github.com>
2025-05-13 22:53:15 +0300

    Update declaration-form.php

--- modules/submit_module/declaration-form.php
+++ modules/submit_module/declaration-form.php
@@ -302,7 +302,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($pdo)) {
                                         </a>
                                     </li>
                                     <li>
-                                        <a class="dropdown-item" href="../favorites_module/favorites.php">
+                                        <a class="dropdown-item" href="../submit_module/favorites.php">
                                             <i class="bi bi-heart"></i> Αγαπημένα
                                         </a>
                                     </li>
