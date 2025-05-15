947e335e259edc36fdc6c2c7ea7c8e8fe9a2ed2f
Stylianos Kyprianou <43478713+stylianosk0@users.noreply.github.com>
2025-05-15 10:56:57 +0300

    Update view-declaration.php

--- modules/submit_module/view-declaration.php
+++ modules/submit_module/view-declaration.php
@@ -175,7 +175,7 @@ $other_incomes = $stmt->fetchAll();
                                         </a>
                                     </li>
                                     <li>
-                                        <a class="dropdown-item" href="../favorites_module/favorites.php">
+                                        <a class="dropdown-item" href="../submit_module/favorites.php">
                                             <i class="bi bi-heart"></i> Αγαπημένα
                                         </a>
                                     </li>
