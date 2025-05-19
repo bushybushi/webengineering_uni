728542658751157e0e839d6d6c56b1b0b1d659d9
antonis10 <andreou261@gmail.com>
2025-05-19 16:55:32 +0300

    Update statistics.php

--- modules/search_module/statistics.php
+++ modules/search_module/statistics.php
@@ -339,7 +339,7 @@ try {
                                     <?php endif; ?>
                                     <?php if (isset($_SESSION['role']) && ($_SESSION['role'] === 'Admin' || $_SESSION['role'] === 'Public' || $_SESSION['role'] === 'Politician')): ?>
                                     <li>
-                                        <a class="dropdown-item" href="../search_module/api_documentation.php">
+                                        <a class="dropdown-item" href="../api_module/api_documentation.php">
                                             <i class="bi bi-code-square"></i> API Documentation
                                         </a>
                                     </li>
@@ -422,7 +422,7 @@ try {
                                     </a>
                                     <?php endif; ?>
                                     <?php if (isset($_SESSION['role']) && ($_SESSION['role'] === 'Admin' || $_SESSION['role'] === 'Public' || $_SESSION['role'] === 'Politician')): ?>
-                                    <a href="../search_module/api_documentation.php" class="nav-link py-2">
+                                    <a href="../api_module/api_documentation.php" class="nav-link py-2">
                                         <i class="bi bi-code-square"></i> API Documentation
                                     </a>
                                     <?php endif; ?>
