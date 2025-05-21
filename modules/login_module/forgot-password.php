e344808f58ef0ea5ba34f9b11816bc5ae3891d1d
Kyriacosa44 <andrkyriacos@gmail.com>
2025-05-21 16:03:49 +0300

    Update forgot-password.php

--- modules/login_module/forgot-password.php
+++ modules/login_module/forgot-password.php
@@ -173,6 +173,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<!-- Favicon -->
+    <link rel="icon" type="image/png" href="../../assets/images/iconlogo.png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forgot Password - ΠΟΘΕΝ ΕΣΧΕΣ</title>
