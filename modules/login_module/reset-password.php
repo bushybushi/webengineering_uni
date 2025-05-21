e58ccebc4e69c22de8c1c46d0063e6c290b8104f
Kyriacosa44 <andrkyriacos@gmail.com>
2025-05-21 16:04:34 +0300

    Update reset-password.php

--- modules/login_module/reset-password.php
+++ modules/login_module/reset-password.php
@@ -92,6 +92,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && empty($token_err)){
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<!-- Favicon -->
+    <link rel="icon" type="image/png" href="../../assets/images/iconlogo.png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reset Password - ΠΟΘΕΝ ΕΣΧΕΣ</title>
