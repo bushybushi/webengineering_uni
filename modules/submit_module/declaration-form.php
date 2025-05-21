1ab36ce9c2cdff58b9884f6e50afc25ffa55b792
Stylianos Kyprianou <43478713+stylianosk0@users.noreply.github.com>
2025-05-21 15:20:14 +0300

    Update declaration-form.php

--- modules/submit_module/declaration-form.php
+++ modules/submit_module/declaration-form.php
@@ -912,6 +912,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($pdo)) {
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Custom JS -->
-    <script src="../../js/main.js"></script>
+    <script src="../../assets/js/main.js"></script>
 </body>
 </html>
